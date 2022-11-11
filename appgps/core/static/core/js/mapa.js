ca48705f4e5efdb5da0760a0a24f711fc34ba9b5
Martin3125 <101206120+Martin3125@users.noreply.github.com>
2022-11-11 04:45:28 -0300

    se arrglo el mapa de arriendo y se agrego algo de js al mapa

--- appgps/core/static/core/js/mapa.js
+++ appgps/core/static/core/js/mapa.js
@@ -1,15 +1,100 @@
 
+// function iniciarMap(){
+//     var coord = {lat:-34.5956145 ,lng: -58.4431949};
+//     var map = new google.maps.Map(document.getElementById('map'),{
+//       zoom: 10,
+//       center: coord
+//     });
+//     var marker = new google.maps.Marker({
+//       position: coord,
+//       map: map
+//     });
+// }
+
+// $(document).ready(function() {
+    
+//     google.maps.event.addDomListener(window, 'load', initMap);
 
 
 
-function iniciarMap(){
-    var coord = {lat:-34.5956145 ,lng: -58.4431949};
-    var map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 10,
-      center: coord
-    });
-    var marker = new google.maps.Marker({
-      position: coord,
-      map: map
-    });
-}
+    
+
+      
+// });      
+
+// Initialize and add the map
+// var map;
+// function initMap() {
+//   // The location of Uluru
+//   const uluru = { lat: -25.344, lng: 131.031 };
+//   // The map, centered at Uluru
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 4,
+//     center: uluru,
+//   });
+//   // The marker, positioned at Uluru
+//   const marker = new google.maps.Marker({
+//     position: uluru,
+//     map: map,
+//   });
+//   var stylesArray = [
+//     {
+//       featureType: '',
+//       elementType: '',
+//       stylers: [
+//         {color: ''},
+//         {visibility: ''},
+//         // Add any stylers you need.
+//       ]
+//     },
+//     {
+//       featureType: '',
+//       // Add the stylers you need.
+//     }
+//   ]
+// }
+
+// window.initMap = initMap;
+
+// $(document).ready(function(){
+//   if (navigator.geolocation)
+//   {
+//       navigator.geolocation.getCurrentPosition(getCoords, getError);
+//   }else{
+
+//   }
+//   var map;
+//   function getCoords(position)
+//   {
+//       var lat = position.coords.latitude;
+//       var lng = position.coords.longitude;
+
+//       initialize(lat, lng);
+
+//   }
+
+//   function getError(err)
+//   {
+//       initialize(-33.69417055239359, -71.21368835681287);
+//   }
+
+//   function initialize (lat, lng)
+//   {
+//       var latleng = new google.maps.LatLng(lat, lng);
+//       var mapSettings = {
+//           center: latleng,
+//           zoom: 14,
+//           mapTypeId: google.maps.mapTypeId.b7011240b82da652
+//       }
+//       map = new google.maps.Map($('#mapa').get(0), mapSettings);
+
+//       var marker = new google.maps.Marker({
+//           position: latleng,
+//           map: map,
+//           draggable: true,
+//           title: 'Arrastrame'
+//       });
+//   }
+// });
+
+
--- appgps/core/views.py
+++ appgps/core/views.py
@@ -2,7 +2,11 @@ from datetime import datetime
 from django.shortcuts import render
 from django.contrib import messages
 from django.shortcuts import redirect, render
-
+# from django.utils import simplejson
+# from django.http import HttpResponse
+# from django.shortcuts import render_to_response
+# from django.template import RequestContext
+# from django.utils.timesince import timesince
 from .models import *
 
 # Create your views here.
@@ -100,6 +104,39 @@ def cerrarSesion(request):
     messages.success(request, 'Sesion Cerrada')
     return render(request, 'core/ajustes.html')
 
+# def arrendar(request):
+#     form = UbicacionForm()
+#     Ubicaciones = Ubicacion.objects.all().order_by('-fecha')
+#     return render_to_response('arriendo.html', {'form': form}, context_instance=RequestContext(request))
+
+# def coords_save(request):
+#     if request.is_ajax():
+#         form = UbicacionForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             Ubicaciones = Ubicacion.objects.all().order_by('-fecha')
+#             data ='<ul>'
+#             for ubicacion in Ubicaciones:
+#                 data += '<li>%s %s -hace %s<li>' %(ubicacion.nombre, ubicacion.user, timesince(ubicacion.fecha))
+#             data +='<ul>'
+#             return HttpResponse(simplejson.dumps({'ok' :True, 'msg':data }), mimetype='application/json')
+#         else:
+#             return HttpResponse(simplejson.dumps({'ok' :False, 'msg': 'Debes llenar todos los campos'}), mimetype='application/json')
 
 
-
+def arriendo(request):
+    if request.method == 'POST':
+        #estructura de condicion que verifica si el usuario que se intenta registrar existe
+        if Ubicacion.objects.filter(nombre = request.POST['nombre']).exists(): # se verifica la existencia por el campo de email
+            messages.success(request, 'El usuario ingresado ya existe')
+        else:
+            #creacion del nuevo usuario, entre [] se coloca el atributo "name" de los input en el html
+            newUB = Ubicacion(
+                nombre = request.POST['nombre'],
+                lat =request.POST['lat'],
+                lng = request.POST['lng'],
+            )
+            newUB.save()
+            messages.success(request, 'Usuario registrado correctamente')
+            return redirect('home')
+    return render(request, 'core/arriendo.html')
